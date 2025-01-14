6561e2067526538b6db3a8e5daa3e2e1b95796b9
Shivang Kumar <shivangkumar@192.168.1.11>
2025-01-14 18:44:39 +0530

    applicaiton error added

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 import express from 'express';
 import userRouter from './src/features/user/user.routes.js';
+import { ApplicationError } from './src/error-handler/applicationError.js';
 
 const server = express();
 server.use(express.json());
 
 server.use('/api', userRouter);
+
+server.use((err, req, res, next) => {
+    if(err instanceof ApplicationError) {
+        res.status(err.code).send({message: err.message});
+    }
+    res.status(500).send('Something went wrong, please try later');
+})
+
+server.use((req, res) => {
+    res.status(404).send('API not found. Please check our documentation for more information at localhost:3200/api-docs');
+})
 export default server;
